6c3d3268ea921f6d4fcaa9c06108786bc7790a72
iqmlhzzq <49804606+iqmlhzzq@users.noreply.github.com>
2019-05-06 00:03:08 +0800

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/register.component';
+import { UserResolver } from './user/user.resolver';
+import { AuthGuard } from './core/auth.guard';
+
+export const rootRouterConfig: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
+  { path: 'user', component: UserComponent,  resolve: { data: UserResolver}}
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBJDXs34dH2GEdqZsLpRD_uDSQn8v7BLrI",
+    authDomain: "luvforyou-7cc1c.firebaseapp.com",
+    databaseURL: "https://luvforyou-7cc1c.firebaseio.com",
+    projectId: "luvforyou-7cc1c",
+    storageBucket: "luvforyou-7cc1c.appspot.com",
+    messagingSenderId: "546552200902"
+  }
+};
